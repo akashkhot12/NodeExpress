303a7491cd0366786bcaf618cf8363d5d01c7f85
akashkhot12 <akashkhot03@gmail.com>
2023-10-23 11:49:29 +0530

    first programe

--- nodejsBAsics/app.js
+++ nodejsBAsics/app.js
@@ -1,3 +1,11 @@
-console.log("hello form node js ");
+// console.log("hello form node js ");
 
-console.log("tgis is my first noode js app");
+const readline  =  require('readline')
+const l1 =  readline.createInterface({
+    input:process.stdin,
+    output:process.stdout
+})
+
+l1.question("please enter your name : ",(name)=>{
+    console.log("your enter key is :" +name);
+})

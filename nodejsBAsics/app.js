c60e8dd6e4e9d7fc76db9647cce4a6b21d33d047
akashkhot12 <akashkhot03@gmail.com>
2023-10-23 12:07:54 +0530

    adding close functions

--- nodejsBAsics/app.js
+++ nodejsBAsics/app.js
@@ -1,4 +1,4 @@
-console.log("hello form node js ");
+// console.log("hello form node js ");
 
 const readline  =  require('readline')
 const l1 =  readline.createInterface({
@@ -8,6 +8,12 @@ const l1 =  readline.createInterface({
 
 l1.question("please enter your name : ",(name)=>{
     console.log("your enter key is :" +name);
+    l1.close();
+})
+
+l1.on('close',()=>{
+    console.log("interface is close");
+    process.exit(0);
 })
 
 // const aksing = require("readline");

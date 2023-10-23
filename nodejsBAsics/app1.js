6bcfa85b38605e6008733c546f13546e9ffa20a7
akashkhot12 <akashkhot03@gmail.com>
2023-10-23 12:31:00 +0530

    read and write file

--- nodejsBAsics/app1.js
+++ nodejsBAsics/app1.js
@@ -2,3 +2,7 @@ const readline  = require('readline')
 const fs =  require('fs')
 let textIn = fs.readFileSync("../files/text.txt",'utf-8')
 console.log(textIn);
+
+
+let content  = ` data read form inpute text : ${textIn} /nDate created ${new Date()}`
+fs.writeFileSync('../files/write.txt',content);

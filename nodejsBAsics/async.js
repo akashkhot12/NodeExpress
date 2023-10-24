561e90cb263ba4570f1fb61b915d971b73d311c9
akashkhot12 <akashkhot03@gmail.com>
2023-10-24 15:00:05 +0530

    file run

--- nodejsBAsics/async.js
+++ nodejsBAsics/async.js
@@ -1,3 +1,11 @@
+const { error } = require('console');
 const fs  = require('fs');
 
-fs.readFile('')
+fs.readFile('../files/starts.txt','utf-8',(error1,data1)=>{
+    console.log(data1);
+    fs.readFile(`../files/${data1}.txt`,'utf-8',(error2,data2)=>{
+        console.log(data2);
+    })
+})
+
+console.log("reading file");

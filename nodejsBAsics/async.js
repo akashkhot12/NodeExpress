5c20a1c84c97e4026128909c7b7608c7ad3fc3aa
akashkhot12 <akashkhot03@gmail.com>
2023-10-24 15:04:29 +0530

    undefinned file show

--- nodejsBAsics/async.js
+++ nodejsBAsics/async.js
@@ -4,7 +4,7 @@ const fs  = require('fs');
 fs.readFile('../files/starts.txt','utf-8',(error1,data1)=>{
     console.log(data1);
     fs.readFile(`../files/${data1}.txt`,'utf-8',(error2,data2)=>{
-        console.log(data2);
+        console.log(data2+" this is data run");
     })
 })
 

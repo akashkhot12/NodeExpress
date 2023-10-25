b2b77d81ad314829ca11bba46c96e225b3c74249
akashkhot12 <akashkhot03@gmail.com>
2023-10-25 14:18:28 +0530

    html file is show

--- nodejsBAsics/req&res.js
+++ nodejsBAsics/req&res.js
@@ -1,13 +1,15 @@
 const http = require('http')
 const fs  =  require('fs')
 
+const html=fs.readFileSync('../files/index.html','utf-8')
+
 const server  = http.createServer((req,res)=>{
-    res.end("<h1>this is your server</h1>")
+    res.end(html)
     console.log("your server is upgrated");
 
 })
 
 server.listen(8000,'127.0.0.1',()=>{
     console.log("your data");
-})
+}) 
 

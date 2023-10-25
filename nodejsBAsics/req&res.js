7492616a2109105de469c135d00d915db467db8c
akashkhot12 <akashkhot03@gmail.com>
2023-10-25 22:21:43 +0530

    formated documents

--- nodejsBAsics/req&res.js
+++ nodejsBAsics/req&res.js
@@ -1,15 +1,13 @@
-const http = require('http')
-const fs  =  require('fs')
+const http = require("http");
+const fs = require("fs");
 
-const html=fs.readFileSync('./index.html','utf-8')
+const html = fs.readFileSync("./index.html", "utf-8");
 
-const server  = http.createServer((req,res)=>{
-    res.end(html)
-    console.log("your server is upgrated");
+const server = http.createServer((req, res) => {
+  res.end(html);
+  console.log("your server is upgrated");
+});
 
-})
-
-server.listen(8000,'127.0.0.1',()=>{
-    console.log("your data");
-}) 
-
+server.listen(8000, "127.0.0.1", () => {
+  console.log("your data");
+});

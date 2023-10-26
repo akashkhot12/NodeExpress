17b56f0c1fcc9cd39a6afd78382ee885a2bad6ba
akashkhot12 <akashkhot03@gmail.com>
2023-10-26 12:47:42 +0530

    usning status code

--- nodejsBAsics/reshtml.js
+++ nodejsBAsics/reshtml.js
@@ -6,15 +6,19 @@ const html  = fs.readFileSync('./resHtml.html','utf-8')
 const server = http.createServer((req,res)=>{
     let path = req.url;
     if (path =='/' || path.toLocaleLowerCase()==='/home') {
+        res.writeHead(200);
         res.end(html.replace('{{%content%}}','your are in home page'))
     }
     else if(path.toLocaleLowerCase()==='/about'){
+        res.writeHead(200);
         res.end(html.replace('{{%content%}}','your are in about page'))
     }
     else if(path.toLocaleLowerCase()==='/contact'){
+        res.writeHead(200);
         res.end(html.replace('{{%content%}}','your are in contact page'))
     }
     else{
+        res.writeHead(404);
         res.end(html.replace('{{%content%}}','error 404: something wrong'))
     }
 })

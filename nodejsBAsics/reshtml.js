e1cd852282495649590ebb0eb2b9da057dfd2f3b
akashkhot12 <akashkhot03@gmail.com>
2023-10-27 10:53:54 +0530

    add contends

--- nodejsBAsics/reshtml.js
+++ nodejsBAsics/reshtml.js
@@ -6,6 +6,18 @@ const { response } = require('express');
 const html  = fs.readFileSync('./resHtml.html','utf-8')
 const product   = JSON.parse(fs.readFileSync('../files/product.json','utf-8'));
 const product_list = fs.readFileSync('../templates/product-list.html','utf-8');
+let productHtmlArray = product.map((prod)=>{
+    let output  = product_list.replace('{{IMAGES}}', prod.productImage);
+    output = product_list.replace('{{%NAME%}}',prod.name);
+    output = product_list.replace('{{%MODELNAME%}}',prod.modelName);
+    output = product_list.replace('{{%MODELNO%}}',prod.modelNumber);
+    output = product_list.replace('{{%SIZE%}}',prod.size);
+    output = product_list.replace('{{%CAMERA%}}',prod.camera);
+    output = product_list.replace('{{%PRICE%}}',prod.price);
+    output = product_list.replace('{{%COLOR%}}',prod.color);
+
+    return output;
+})
 
 const server = http.createServer((req,res)=>{
     let path = req.url;
@@ -14,11 +26,11 @@ const server = http.createServer((req,res)=>{
             'Content-type':'text/html',
             'my-header': 'hello,world'
         });
-        res.end(html.replace('{{%content%}}','your are in home page'))
+        res.end(html.replace('{{%content%}}',product_list))
     }
     else if(path.toLocaleLowerCase()==='/about'){
         res.writeHead(200);
-        res.end(html.replace('{{%content%}}',product_list))
+        res.end(html.replace('{{%content%}}','your are in about page'))
     }
     else if(path.toLocaleLowerCase()==='/contact'){
         res.writeHead(200);

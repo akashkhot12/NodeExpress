cfb6a92614c0116d35cb83fd3e3982f7ba9d7cda
akashkhot12 <akashkhot03@gmail.com>
2023-10-27 11:44:25 +0530

    project html

--- projects/project.js
+++ projects/project.js
@@ -1,7 +1,15 @@
 const http = require('http');
 const fs = require('fs');
 
+// files path 
+
+let html  = fs.readFileSync('project.html','utf-8');
+let product = json.parse(fs.readFileSync('project.json','utf-8'));
+let projectList = fs.readFileSync('project_list.html','utf-8');
+
+
 let server = http.createServer((req,res)=>{
+    let path = req.url;
     console.log("server is waiting");
 })
  server.listen(9000,'127.0.0.1',()=>{

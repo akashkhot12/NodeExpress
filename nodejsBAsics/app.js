89afb1f32ae1e1543e8269bc5053cff020dd1fee
akashkhot12 <akashkhot03@gmail.com>
2023-10-29 10:53:55 +0530

    app node

--- nodejsBAsics/app.js
+++ nodejsBAsics/app.js
@@ -1,20 +1,20 @@
 // console.log("hello form node js ");
 
-// const readline  =  require('readline')
-// const l1 =  readline.createInterface({
-//     input:process.stdin,
-//     output:process.stdout
-// })
+const readline  =  require('readline')
+const l1 =  readline.createInterface({
+    input:process.stdin,
+    output:process.stdout
+})
 
-// l1.question("please enter your name : ",(name)=>{
-//     console.log("your enter key is :" +name);
-//     l1.close();
-// })
+l1.question("please enter your name : ",(name)=>{
+    console.log("your enter key is :" +name);
+    l1.close();
+})
 
-// l1.on('close',()=>{
-//     console.log("interface is close");
-//     process.exit(0);
-// })
+l1.on('close',()=>{
+    console.log("interface is close");
+    process.exit(0);
+})
 
 // const aksing = require("readline");
 // const p1 = aksing.createInterface({

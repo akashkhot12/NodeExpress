65ea10c7b6af0efb1b662dca8e2f0236e6262fe6
akashkhot12 <akashkhot03@gmail.com>
2023-10-31 22:17:19 +0530

    creatting server

--- 4LinesApi.js/app.js
+++ 4LinesApi.js/app.js
@@ -1,11 +1,4 @@
 const { write } = require('fs');
 const http = require('http');
 
-const server = http.serverDAta((res,req)=>{
-    console.log("app is running");
-    console.log(res);
-   res.end("this is server")
-    
-}).listen('7000',()=>{
-    console.log("my app is open");
-})
+const server = http

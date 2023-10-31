591f7954f7f5c3c0d825786dbb2a5300f08af3c5
akashkhot12 <akashkhot03@gmail.com>
2023-10-31 22:10:49 +0530

    generate port

--- 4LinesApi.js/index.js
+++ 4LinesApi.js/index.js
@@ -1,12 +1,9 @@
-const app = require('express')(); 
+const app = require("express")();
 
 const port = 8000;
 
-app.listen(
-    port,
-    ()=>console.log(`server running on ${port}`)
-);
+app.listen(port, () => console.log(`server running on ${port}`));
 
-app.get('/fru',(req,res)=>{
-    res.send("mango and banana")
-})
+app.get("/fru", (req, res) => {
+  res.send("mango and banana");
+});

ddada4360fc86cf63ebf2d8b7dceed727d353fe5
akashkhot12 <akashkhot03@gmail.com>
2023-10-31 22:19:59 +0530

    create server

--- 4LinesApi.js/app.js
+++ 4LinesApi.js/app.js
@@ -1,4 +1,10 @@
 const { write } = require('fs');
 const http = require('http');
 
-const server = http
+const server = http.createServer((req,res)=>{
+    console.log('server is running');
+    res.end('this is website')
+})
+server.listen(4500,'127.0.0.1',()=>{
+    console.log("application oper");
+})

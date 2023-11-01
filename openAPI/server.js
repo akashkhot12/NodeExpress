c4cf7644f78a8cb2c6f26b48fe208cefe7d681f9
akashkhot12 <akashkhot03@gmail.com>
2023-11-01 10:49:41 +0530

    humidity show

--- openAPI/server.js
+++ openAPI/server.js
@@ -4,12 +4,12 @@ const request = require('request')
 const app = express();
 
 app.get('/' , (req , res)=>{
-    let city = req.query.city
+    let city = req.query.city;
     const request = require('request');
     request(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=4f63620a24e38984dde9c0ab2b54ca76`, function (error, response, body) {
      if (response.statusCode==200) {
         let data = JSON.parse(body)
-        res.send(`the weather in you city "${city}" is ${data.wheather.description}`)
+        res.send(`the weather in you city "${city}" is ${data.weather[0].description}, and humidity is ${data.main.humidity}`)
      }
       console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
       console.log('body:', body); // Print the HTML for the Google homepage.
